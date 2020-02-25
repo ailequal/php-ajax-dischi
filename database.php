216442865a7bdf75170b1794bf069744b79b98e9
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-25 17:39:05 +0100

    Added more data to the database.php

--- database.php
+++ database.php
@@ -17,6 +17,84 @@
 			'author' => 'Death Cab for Cutie',
 			'year' => 2003,
 			'poster' => 'https://i.scdn.co/image/ab67616d0000b273e9ee9a226a3ef6d6e26fbe67'
+		],
+		[
+			'title' => 'Kintsugi',
+			'author' => 'Death Cab for Cutie',
+			'year' => 2015,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b27343aafb2edd88b9447f49337d'
+		],
+		[
+			'title' => 'We Have the Facts and We\'re Voting Yes',
+			'author' => 'Death Cab for Cutie',
+			'year' => 2000,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b27350586d947335e6a1b23534ab'
+		],
+		[
+			'title' => 'Coexist',
+			'author' => 'The xx',
+			'year' => 2012,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b27372770e0507fc3a46e47daf95'
+		],
+		[
+			'title' => 'I See You',
+			'author' => 'The xx',
+			'year' => 2017,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b2735f5dc0f546406ec5a96018bb'
+		],
+		[
+			'title' => 'Love Is Dead',
+			'author' => 'CHVRCHES',
+			'year' => 2018,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b2739ca7eced01a249c1ea5fe7e1'
+		],
+		[
+			'title' => 'Every Open Eye',
+			'author' => 'CHVRCHES',
+			'year' => 2015,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b273839f2d4c4a2b99c8abd42b5f'
+		],
+		[
+			'title' => 'Everyday Life',
+			'author' => 'Coldplay',
+			'year' => 2019,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b2737b9a76ec264401b223e607a4'
+		],
+		[
+			'title' => 'Ghost Stories',
+			'author' => 'Coldplay',
+			'year' => 2014,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b2731cbd0d5849b51c79c99e7b87'
+		],
+		[
+			'title' => 'A Rush of Blood to the Head',
+			'author' => 'Coldplay',
+			'year' => 2002,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b273ad6f442ecac8735b9285a923'
+		],
+		[
+			'title' => 'Random Access Memories',
+			'author' => 'Daft Punk',
+			'year' => 2013,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b2731d97ca7376f835055f828139'
+		],
+		[
+			'title' => 'Human After All',
+			'author' => 'Daft Punk',
+			'year' => 2005,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b2732ed719bad67261c7bf090c70'
+		],
+		[
+			'title' => 'Loud Like Love',
+			'author' => 'Placebo',
+			'year' => 2013,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b273e5d878593752fa6c6190314d'
+		],
+		[
+			'title' => 'Meds',
+			'author' => 'Placebo',
+			'year' => 2006,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b2735a95bd5fa1b59b390a22d1de'
 		]
 	]
 ?>
