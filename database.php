e55f471db529e956151baf21ee0f3d98c8bbbe6a
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-25 17:20:18 +0100

    PHP script is working

--- database.php
+++ database.php
@@ -5,6 +5,18 @@
 			'author' => 'Bon Jovi',
 			'year' => 1988,
 			'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+		],
+		[
+			'title' => 'Plans',
+			'author' => 'Death Cab for Cutie',
+			'year' => 2008,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b273977f49e4a7170bdd03fb429b'
+		],
+		[
+			'title' => 'Transatlanticism',
+			'author' => 'Death Cab for Cutie',
+			'year' => 2003,
+			'poster' => 'https://i.scdn.co/image/ab67616d0000b273e9ee9a226a3ef6d6e26fbe67'
 		]
 	]
 ?>
