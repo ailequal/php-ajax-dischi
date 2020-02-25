32dafb951620baacb359b40628b886c5f1eda5aa
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-25 15:56:24 +0100

    Added file structure

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+	$database = [
+		[
+			'title' => 'New Jersey',
+			'author' => 'Bon Jovi',
+			'year' => 1988,
+			'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+		]
+	]
+?>
